84d0ff1cf629ed4b6ca97f59b8d8eb99ffc9a531
Gabriel Gonçalves 👨🏻‍💻 <gabrielgmamorim@gmail.com>
2026-05-08 22:33:35 -0300

    Update left-join.sql

--- joins/left-join.sql
+++ joins/left-join.sql
@@ -1,1 +1,11 @@
+--left joins returns ALL records from the left table, 
+-- even when there is no matching record in the right table.
+-- Non-matching values from the right table become NULL.
 
+
+--1 Here we show all pets, including pets without registered owners.
+
+SELECT p.nome, c.nome
+FROM pets p
+LEFT JOIN clientes c
+    ON c.id = p.cliente_id;
