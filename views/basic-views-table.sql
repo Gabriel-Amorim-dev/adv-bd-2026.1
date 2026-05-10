ceff8a5ee156ff58ff4e6b23a080fe1428fb00bf
Gabriel Gonçalves 👨🏻‍💻 <gabrielgmamorim@gmail.com>
2026-05-10 20:52:43 -0300

    Add view for total registered animals per breed

--- views/basic-views-table.sql
+++ views/basic-views-table.sql
@@ -10,3 +10,15 @@ where p.especie = 'cachorro';
 
 -- 2 After being created, the view is stored in our database, so we simply need to call it whenever we need to search for all the dogs in our database
 select * from Cachorros
+
+-- 3 This time, we will create a complex view with aggregation!
+-- 4 As you can see, we name our view "vw_total_por_raca", here, we shall be returning the total of registred animals per breed.
+create view vw_total_por_raca as 
+SELECT 
+    raca, 
+    COUNT(*) AS total_por_raca
+FROM pets
+GROUP BY raca
+order by total_por_raca desc;
+-- 5 Then, we simply call our created view.
+select * from vw_total_por_raca
